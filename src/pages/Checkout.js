9bc6205ea0b3cd418b34487d3a40bc5edc68b7f1
khussa1n <husain.khudaybergenov@gamil.com>
2022-06-15 22:01:36 +0600

    fixed bugs

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -10,8 +10,10 @@ import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Container from '@mui/material/Container';
+import Footer from '../components/Footer';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme) => (
+  {
   root: {
     display: 'flex',
     alignItems: 'center',
@@ -41,146 +43,173 @@ const useStyles = makeStyles((theme) => ({
     marginTop: -12,
     marginLeft: -12,
   },
-}));
+}
+));
 
 export default function Checkout() {
   const classes = useStyles();
   const [loading, setLoading] = React.useState(false);
   const [success, setSuccess] = React.useState(false);
-  const timer = React.useRef();
+
+  // for submit
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [address, setAddress] = React.useState("");
+  const [city, setCity] = React.useState("");
+  const [state, setState] = React.useState("");
+  const [country, setCountry] = React.useState("");
+  const [phone, setPhone] = React.useState("");
+  const [message, setMessage] = React.useState("");
 
   const buttonClassname = clsx({
     [classes.buttonSuccess]: success,
   });
 
-  React.useEffect(() => {
-    return () => {
-      clearTimeout(timer.current);
-    };
-  }, []);
-
   const handleButtonClick = () => {
     if (!loading) {
       setSuccess(false);
       setLoading(true);
-      timer.current = window.setTimeout(() => {
-        setSuccess(true);
-        setLoading(false);
-      }, 2000);
+      
+    }
+
+    if (true) {
+      setSuccess(true);
+      setLoading(false);
     }
   };
   return (
-    <Container component="main" maxWidth="sm" sx={{ mb: 4, mt: 4 }}>  
-    <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Shipping address
-      </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="firstName"
-            name="firstName"
-            label="First name"
-            fullWidth
-            autoComplete="given-name"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="lastName"
-            name="lastName"
-            label="Last name"
-            fullWidth
-            autoComplete="family-name"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="address"
-            name="address"
-            label="Address line"
-            fullWidth
-            autoComplete="shipping address-line"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="city"
-            name="city"
-            label="City"
-            fullWidth
-            autoComplete="shipping address-level2"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="state"
-            name="state"
-            label="State/Province/Region"
-            fullWidth
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="country"
-            name="country"
-            label="Country"
-            fullWidth
-            autoComplete="shipping country"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="phone"
-            name="phone"
-            label="Phone"
-            fullWidth
-            autoComplete="phone"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="message"
-            name="message"
-            label="Message"
-            fullWidth
-            autoComplete="message"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-            label="Use this address for payment details"
-          />
-        </Grid>
-      </Grid>
-      <div className={classes.wrapper}>
-        <Button
-          variant="contained"
-          color="primary"
-          className={buttonClassname}
-          disabled={loading}
-          onClick={handleButtonClick}
-        >
-          {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
-          Checkout
-        </Button>
+    <>
+      <div style={{backgroundImage:"linear-gradient(to right, #00C2FF, #019CF3)"}}>
+        <Container component="main" maxWidth="sm" sx={{ py: 4}}>  
+          <React.Fragment>
+            <Typography variant="h6" gutterBottom>
+              Shipping address
+            </Typography>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  required
+                  id="firstName"
+                  name="firstName"
+                  label="First name"
+                  fullWidth
+                  autoComplete="given-name"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  required
+                  id="lastName"
+                  name="lastName"
+                  label="Last name"
+                  fullWidth
+                  autoComplete="family-name"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                  id="address"
+                  name="address"
+                  label="Address line"
+                  fullWidth
+                  autoComplete="shipping address-line"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                  required
+                  id="city"
+                  name="city"
+                  label="City"
+                  fullWidth
+                  autoComplete="shipping address-level2"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  value={state}
+                  onChange={(e) => setState(e.target.value)}
+                  id="state"
+                  name="state"
+                  label="State/Province/Region"
+                  fullWidth
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  value={country}
+                  onChange={(e) => setCountry(e.target.value)}
+                  required
+                  id="country"
+                  name="country"
+                  label="Country"
+                  fullWidth
+                  autoComplete="shipping country"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  required
+                  id="phone"
+                  name="phone"
+                  label="Phone"
+                  fullWidth
+                  autoComplete="phone"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  required
+                  id="message"
+                  name="message"
+                  label="Message"
+                  fullWidth
+                  autoComplete="message"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
+                  label="Use this address for delivery details"
+                />
+              </Grid>
+            </Grid>
+            <div className={classes.wrapper}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={buttonClassname}
+                disabled={loading}
+                onClick={handleButtonClick}
+              >
+                {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
+                Checkout
+              </Button>
+            </div>
+          </React.Fragment>
+        </Container>
       </div>
-    </React.Fragment>
-    </Container>
+      <Footer />
+    </>
   );
 }
--- src/pages/PostBook.js
+++ src/pages/PostBook.js
@@ -1,9 +1,229 @@
-import React from 'react'
+import * as React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { green } from '@material-ui/core/colors';
+import Button from '@material-ui/core/Button';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Container from '@mui/material/Container';
+import Footer from '../components/Footer';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import ListItemText from '@mui/material/ListItemText';
+import Select from '@mui/material/Select';
 
+const useStyles = makeStyles((theme) => (
+  {
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: 'relative',
+  },
+  buttonSuccess: {
+    backgroundColor: green[500],
+    '&:hover': {
+      backgroundColor: green[700],
+    },
+  },
+  fabProgress: {
+    color: green[500],
+    position: 'absolute',
+    top: -6,
+    left: -6,
+    zIndex: 1,
+  },
+  buttonProgress: {
+    color: green[500],
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12,
+  },
+}
+));
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  'Oliver Hansen',
+  'Van Henry',
+  'April Tucker',
+  'Ralph Hubbard',
+  'Omar Alexander',
+  'Carlos Abbott',
+  'Miriam Wagner',
+  'Bradley Wilkerson',
+  'Virginia Andrews',
+  'Kelly Snyder',
+];
 export default function PostBook() {
+  const classes = useStyles();
+  const [loading, setLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+
+  // for submit
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [address, setAddress] = React.useState("");
+  const [city, setCity] = React.useState("");
+  const [state, setState] = React.useState("");
+  const [country, setCountry] = React.useState("");
+  const [phone, setPhone] = React.useState("");
+
+  // description
+  const [value, setValue] = React.useState('Controlled');
+  const handleChangeDescription = (event) => {
+    setValue(event.target.value);
+  };
+
+  // categories
+  const [personName, setPersonName] = React.useState([]);
+  const handleChangeCategories = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
+  const buttonClassname = clsx({
+    [classes.buttonSuccess]: success,
+  });
+
+  const handleButtonClick = () => {
+    if (!loading) {
+      setSuccess(false);
+      setLoading(true);
+      
+    }
+
+    if (true) {
+      setSuccess(true);
+      setLoading(false);
+    }
+  };
   return (
-    <div>
-        
-    </div>
-  )
+    <>
+      <div style={{backgroundImage:"linear-gradient(to right, #00C2FF, #019CF3)"}}>
+        <Container component="main" maxWidth="sm" sx={{ py: 4}}>  
+          <React.Fragment>
+            <Typography variant="h6" gutterBottom>
+              SUBMIT BOOK
+            </Typography>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  required
+                  id="name"
+                  name="name"
+                  label="Title"
+                  fullWidth
+                  autoComplete="given-name"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  required
+                  id="author"
+                  name="author"
+                  label="Author"
+                  fullWidth
+                  autoComplete="family-name"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="standard-multiline-flexible"
+                  label="Description"
+                  multiline
+                  required
+                  fullWidth
+                  maxRows={4}
+                  value={value}
+                  onChange={handleChangeDescription}
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <FormControl sx={{ m: 1, width: 250 }}>
+                  <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
+                  <Select
+                    labelId="demo-multiple-checkbox-label"
+                    id="demo-multiple-checkbox"
+                    multiple
+                    required
+                    value={personName}
+                    onChange={handleChangeCategories}
+                    input={<OutlinedInput label="Tag" />}
+                    renderValue={(selected) => selected.join(', ')}
+                    MenuProps={MenuProps}
+                  >
+                    {names.map((name) => (
+                      <MenuItem key={name} value={name} style={{display: "flex", height: "50px"}}>
+                        <Checkbox checked={personName.indexOf(name) > -1}/>
+                        <ListItemText primary={name} />
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  value={state}
+                  onChange={(e) => setState(e.target.value)}
+                  id="state"
+                  name="state"
+                  required
+                  label="Number of Pages"
+                  fullWidth
+                  variant="standard"
+                />
+              </Grid>
+            </Grid>
+            <div className={classes.wrapper}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={buttonClassname}
+                disabled={loading}
+                onClick={handleButtonClick}
+              >
+                {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
+                  SUBMIT  
+              </Button>
+            </div>
+          </React.Fragment>
+        </Container>
+      </div>
+      <Footer />
+    </>
+  );
 }
